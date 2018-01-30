346d533dbe30d2748448a457ae7055cface4b997
Aditya Verma <adi004.zz@gmail.com>
2018-01-30 22:06:08 +0530

    Completed basic design, scrached anime.js for now

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+
+$(document).ready(function(){
+
+  function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
     var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
   
     return {
@@ -26,4 +29,76 @@ function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
     // document.getElementById("arc1").setAttribute("d", describeArc(75, 75, 50, 0, 359));   // When width < 500px
     document.getElementById("arc1").setAttribute("d", describeArc(150, 150, 130, 0, 359));
   };
+
+
+// $(".intro").fadeIn(3000, function(){
+//    setTimeout(() => {
+//     $(".intro h1").fadeOut(3000);
+//    }, 1000);
+// })
+ 
+$(".intro").css("display", "block").addClass("animated lightSpeedIn")
+
+$(".intro").on("click", function(){
+  $(this).addClass("animated lightSpeedOut")
+  setTimeout(() => {
+    $(".intro").css("display", "none");
+    $(".mode").css("display", "block").addClass("animated flipInX")
+  }, 2000);
+  // $("#red").animate({backgroundColor : "red"}, 2000);
   
+});
+
+$("#easy").on("click", function(){
+  $(".mode").addClass("animated flipOutX	")
+  $("#red").animate({backgroundColor : "red"}, 2000, function(){
+    $("#yellow").animate({backgroundColor : "yellow"}, 1000, function(){
+      $("#green").animate({backgroundColor : "green"}, 1000, function(){
+        $("#blue").animate({backgroundColor : "blue"}, 1000, function(){
+          $(".counter").css("display", "block").addClass("animated", "tada");
+        });
+      });
+    });
+  });
+})
+
+
+
+
+  // $('.ml11 .letters').each(function(){
+  //   $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+  // });
+  
+//   anime.timeline({loop: false})
+//     .add({
+//       targets: '.ml11 .line',
+//       scaleY: [0,1],
+//       opacity: [0.5,1],
+//       easing: "easeOutExpo",
+//       duration: 1000
+//     })
+//     .add({
+//       targets: '.ml11 .line',
+//       translateX: [0,$(".ml11 .letters").width()],
+//       easing: "easeOutExpo",
+//       duration: 1000,
+//       delay: 100
+//     }).add({
+//       targets: '.ml11 .letter',
+//       opacity: [0,1],
+//       easing: "easeOutExpo",
+//       duration: 1000,
+//       offset: '-=775',
+//       delay: function(el, i) {
+//         return 34 * (i+1)
+//       }
+//     }).add({
+//       targets: '.ml11',
+//       opacity: 0,
+//       duration: 1000,
+//       easing: "easeOutExpo",
+//       delay: 1000
+//     });
+  
+
+});
