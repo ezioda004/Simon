cdc0c65b6c507657925dea462b1d4b8f4cb1c20b
Aditya Verma <adi004.zz@gmail.com>
2018-01-30 13:00:05 +0530

    Added svg, trying out anime.js

---
+++ script.js
@@ -0,0 +1,29 @@
+function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+    var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+  
+    return {
+      x: centerX + (radius * Math.cos(angleInRadians)),
+      y: centerY + (radius * Math.sin(angleInRadians))
+    };
+  }
+  
+  function describeArc(x, y, radius, startAngle, endAngle){
+  
+      var start = polarToCartesian(x, y, radius, endAngle);
+      var end = polarToCartesian(x, y, radius, startAngle);
+  
+      var largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
+  
+      var d = [
+          "M", start.x, start.y, 
+          "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y
+      ].join(" ");
+  
+      return d;       
+  }
+  
+  window.onload = function() {
+    // document.getElementById("arc1").setAttribute("d", describeArc(75, 75, 50, 0, 359));   // When width < 500px
+    document.getElementById("arc1").setAttribute("d", describeArc(150, 150, 130, 0, 359));
+  };
+  
