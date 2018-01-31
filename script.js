410adf098c8d792d908f6bc0c14cfe2d11eb1391
Aditya Verma <adi004.zz@gmail.com>
2018-01-31 14:54:01 +0530

    Added some easy mode logic

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 
 $(document).ready(function(){
 
+  let sequence = [];
+  let score = 0;
+  let gameCheck = false;
+  let checkingIndex = 0;
+
+//SVG animation
+
   function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
     var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
   
@@ -29,33 +36,41 @@ $(document).ready(function(){
     // document.getElementById("arc1").setAttribute("d", describeArc(75, 75, 50, 0, 359));   // When width < 500px
     document.getElementById("arc1").setAttribute("d", describeArc(150, 150, 130, 0, 359));
   };
-
-
-// $(".intro").fadeIn(3000, function(){
-//    setTimeout(() => {
-//     $(".intro h1").fadeOut(3000);
-//    }, 1000);
-// })
  
+//Intro animation
+
 $(".intro").css("display", "block").addClass("animated lightSpeedIn")
 
 $(".intro").on("click", function(){
   $(this).addClass("animated lightSpeedOut")
   setTimeout(() => {
     $(".intro").css("display", "none");
-    $(".mode").css("display", "block").addClass("animated flipInX")
+    $(".mode").css("display", "block").addClass("animated flipInX");
   }, 2000);
   // $("#red").animate({backgroundColor : "red"}, 2000);
   
 });
 
+//Easy mode animation
+
 $("#easy").on("click", function(){
-  $(".mode").addClass("animated flipOutX	")
+  $(".mode").addClass("animated flipOutX");
   $("#red").animate({backgroundColor : "red"}, 2000, function(){
     $("#yellow").animate({backgroundColor : "yellow"}, 1000, function(){
       $("#green").animate({backgroundColor : "green"}, 1000, function(){
         $("#blue").animate({backgroundColor : "blue"}, 1000, function(){
           $(".counter").css("display", "block").addClass("animated", "tada");
+          setTimeout(function(){
+            easyMode();
+            setTimeout(function(){
+              gameCheck = true;
+            checker();
+            }, 1000)
+            
+          }, 2000)
+          
+          
+          
         });
       });
     });
@@ -63,42 +78,96 @@ $("#easy").on("click", function(){
 })
 
 
+//Easy mode
 
 
-  // $('.ml11 .letters').each(function(){
-  //   $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
-  // });
+function easyMode(){
+  score+=1;
+  $(".counter span").html(score);
+  sequence.push(Math.floor(Math.random()*4));
   
-//   anime.timeline({loop: false})
-//     .add({
-//       targets: '.ml11 .line',
-//       scaleY: [0,1],
-//       opacity: [0.5,1],
-//       easing: "easeOutExpo",
-//       duration: 1000
-//     })
-//     .add({
-//       targets: '.ml11 .line',
-//       translateX: [0,$(".ml11 .letters").width()],
-//       easing: "easeOutExpo",
-//       duration: 1000,
-//       delay: 100
-//     }).add({
-//       targets: '.ml11 .letter',
-//       opacity: [0,1],
-//       easing: "easeOutExpo",
-//       duration: 1000,
-//       offset: '-=775',
-//       delay: function(el, i) {
-//         return 34 * (i+1)
-//       }
-//     }).add({
-//       targets: '.ml11',
-//       opacity: 0,
-//       duration: 1000,
-//       easing: "easeOutExpo",
-//       delay: 1000
-//     });
+  sequence.forEach(function(val){
+    if (val === 0){
+      $("#greenS").get(0).play();
+    }
+    else if (val === 1){
+      $("#redS").get(0).play();
+    }
+    else if (val === 2){
+      $("#yellowS").get(0).play();
+    }
+    else if (val === 3){
+      $("#blueS").get(0).play();
+    }
+  });
   
+}
+
+
+function checker(){
+  console.log("working");
+  if (gameCheck){
+    // for (let i = 0; i < sequence.length; i++){
+    //   if (sequence[i] === 0){
+
+    //   }
+    // }
+    $("#green").on("click", function(e){
+      e.preventDefault();
+      $("#greenS").get(0).play();
+      if (sequence[checkingIndex] === 0){
+        checkingIndex+=1;
+        setTimeout(() => {
+          easyMode();
+        }, 2000);
+        
+      }
+      else {
+        alert("wrong!");
+      }
+    });
+    $("#red").on("click", function(e){
+      e.preventDefault();
+        $("#redS").get(0).play();
+        if (sequence[checkingIndex] === 1){
+          checkingIndex+=1;
+          setTimeout(() => {
+            easyMode();
+          }, 2000);
+        }
+        else {
+          alert("wrong!");
+        }
+      });
+    $("#yellow").on("click", function(e){
+      e.preventDefault();
+      $("#yellowS").get(0).play();
+      if (sequence[checkingIndex] === 2){
+        checkingIndex+=1;
+        setTimeout(() => {
+          easyMode();
+        }, 2000);
+      }
+      else {
+        alert("wrong!");
+      }
+        });
+    $("#blue").on("click", function(e){
+      e.preventDefault();
+      $("#blueS").get(0).play();
+      if (sequence[checkingIndex] === 3){
+        checkingIndex+=1;
+        setTimeout(() => {
+          easyMode();
+        }, 2000);
+      }
+      else {
+        alert("wrong!");
+      }
+      });
+  }
+}
 
 });
+
+
